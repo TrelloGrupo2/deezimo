ca4be5e2fd7bdefc0d9193db7fcde818d40b699c
Kauã Werle <kauawerle19@gmail.com.br>
2023-10-08 20:42:20 -0300

    remove tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <header>
+          <h1 className="text-red">text</h1>
+        </header>
+      </div>
+    </>
+  );
+}
+
+export default App;
