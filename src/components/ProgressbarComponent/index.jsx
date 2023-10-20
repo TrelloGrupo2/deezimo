6df3b88033d570e5b6bd0c0fee8c682bba573010
Kauã Werle <kauawerle19@gmail.com.br>
2023-10-20 16:10:42 -0300

    feat:implement component progressBar

--- src/components/ProgressbarComponent/index.jsx
+++ src/components/ProgressbarComponent/index.jsx
@@ -20,27 +20,3 @@ export const ProgressBar = (props) => {
     </Wrapper>
   );
 };
-
-// const ProgressBarFlexible = (props) => {
-//   const { valuePercentage: value } = props;
-//   const fillerRelativePercentage = (100 / value) * 100;
-//   const fillerFlex = value > 0 ? value / 100 : 0;
-
-//   return (
-//     <div
-//       className="wrapper"
-//       role="progressbar"
-//       aria-valuemin={0}
-//       aria-valuemax={100}
-//       aria-valuenow={value}
-//     >
-//       <div className="barContainer" style={{ flex: fillerFlex }}>
-//         <div
-//           className="fillerBackground"
-//           style={{ width: `${fillerRelativePercentage}%` }}
-//         />
-//       </div>
-//       <div className="textValue">{`${value}%`}</div>
-//     </div>
-//   );
-// };
