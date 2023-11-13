38ddecf2be4025c8fdbc54c369e5c387ab41d7ac
Kauã Werle <kauawerle19@gmail.com.br>
2023-11-13 09:48:43 -0300

    fix: fix layout

--- src/pages/Landing_Page/index.jsx
+++ src/pages/Landing_Page/index.jsx
@@ -1,85 +1,73 @@
 import {
   Content,
-  Header,
-  HeaderContainer,
-  HeaderLink,
-  LinksContainer,
-  ListLinks,
+  ContentBody,
+  Elemento,
+  HIWContent,
+  HIWImages,
+  HIWImgContents,
+  HIWText,
   SloganContainer,
   StartButton,
   StartButtonContainer,
-  TitleContainer,
-  TitleContainerSlogan,
-  YourWishlistButton,
-  YourWishlistButtonContainer,
-  Elemento,
-  HIWContent,
-  HIWText,
-  HIWImages,
-  HIWImgContents,
+  TitleContainerSlogan
 } from "./style";
 
-import img1 from "./img/img1.png"
-import img2 from "./img/img2.png"
-import img3 from "./img/img3.png"
+import { HeaderComponent } from "../../components/Header";
+import img1 from "./img/img1.png";
+import img2 from "./img/img2.png";
+import img3 from "./img/img3.png";
 export function Lp() {
   return (
     <>
-      <Header>
-        <HeaderContainer>
-          <TitleContainer>
-            <h1>Deezimo</h1>
-          </TitleContainer>
-          <LinksContainer>
-            <ListLinks>
-              <li>
-                <HeaderLink>
-                  Contato
-                </HeaderLink>
-              </li>
-              <li>
-                <HeaderLink href="../Login">
-                  Entrar
-                </HeaderLink>
-              </li>
-              <YourWishlistButtonContainer>
-                <YourWishlistButton>
-                  Sua Wishlist
-                </YourWishlistButton>
-              </YourWishlistButtonContainer>
-            </ListLinks>
-          </LinksContainer>
-        </HeaderContainer>
-      </Header>
+      <HeaderComponent />
 
       <Content>
-        <SloganContainer>
-          <TitleContainerSlogan>
-            <h1>
-              Deezimo, Sua plataforma de doacoes online
-            </h1>
-          </TitleContainerSlogan>
-          <StartButtonContainer>
-            <StartButton>Começar</StartButton>
-          </StartButtonContainer>
-        </SloganContainer>
-        <Elemento>
-          <svg width="539" height="811" viewBox="0 0 539 811" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M538.018 439C538.018 661.847 494.504 810.5 304.518 810.5C114.532 810.5 123.822 699.347 123.822 476.5C123.822 253.653 -116.968 0 73.0181 0C263.004 0 538.018 216.153 538.018 439Z" fill="#2A41FA" />
-          </svg>
-        </Elemento>
-        
+        <ContentBody>
+          <SloganContainer>
+            <TitleContainerSlogan>
+              <h1>Deezimo, Sua plataforma de doações online</h1>
+            </TitleContainerSlogan>
+            <StartButtonContainer>
+              <StartButton>Começar</StartButton>
+            </StartButtonContainer>
+          </SloganContainer>
+          <Elemento>
+            <svg
+              width="539"
+              height="811"
+              viewBox="0 0 539 811"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M538.018 439C538.018 661.847 494.504 810.5 304.518 810.5C114.532 810.5 123.822 699.347 123.822 476.5C123.822 253.653 -116.968 0 73.0181 0C263.004 0 538.018 216.153 538.018 439Z"
+                fill="#2A41FA"
+              />
+            </svg>
+          </Elemento>
+        </ContentBody>
       </Content>
       <HIWContent>
         <h1>Como Funciona?</h1>
         <HIWText>
-          <p>No Deezimo, você pode reunir sua lista de desejos de diferentes sites, para receber presentes de outras pessoas! Assim, você pode escolher os produtos que mais gosta e facilitar a vida de quem quer te agradar.</p>
+          <p>
+            No Deezimo, você pode reunir sua lista de desejos de diferentes
+            sites, para receber presentes de outras pessoas! Assim, você pode
+            escolher os produtos que mais gosta e facilitar a vida de quem quer
+            te agradar.
+          </p>
         </HIWText>
       </HIWContent>
       <HIWImages>
-        <HIWImgContents><img src={img3} alt="img1" /></HIWImgContents>
-        <HIWImgContents><img src={img2} alt="" /></HIWImgContents>
-        <HIWImgContents><img src={img1} alt="" /></HIWImgContents>
+        <HIWImgContents>
+          <img src={img3} alt="img1" />
+        </HIWImgContents>
+        <HIWImgContents>
+          <img src={img2} alt="" />
+        </HIWImgContents>
+        <HIWImgContents>
+          <img src={img1} alt="" />
+        </HIWImgContents>
       </HIWImages>
     </>
   );
--- src/pages/Landing_Page/style.js
+++ src/pages/Landing_Page/style.js
@@ -1,173 +1,172 @@
 import styled from "styled-components";
 
-
-
 export const Header = styled.header`
-    padding: 2em 5em;
-
-`
+  padding: 2em 5em;
+`;
 export const TitleContainer = styled.div`
-    h1{
-        color: white;
-    }
-`
+  h1 {
+    color: white;
+  }
+`;
 export const LinksContainer = styled.div`
-    width: 23%;
-
-`
+  width: 23%;
+`;
 export const ListLinks = styled.ul`
-    display: flex;
-    width: 100%;
-    justify-content: space-around;
-    align-items: center ;
-     li{
-        list-style: none;
-    }
-
-`
+  display: flex;
+  width: 100%;
+  justify-content: space-around;
+  align-items: center;
+  li {
+    list-style: none;
+  }
+`;
 export const yourWishlistButton = styled.a`
-    background-color: #5B606B;
-    padding: 0.8em 0.8em;
-    border-radius: 20px;
-
-`
+  background-color: #5b606b;
+  padding: 0.8em 0.8em;
+  border-radius: 20px;
+`;
 
 export const HeaderLp = styled.header`
-    padding: 2em 5em;
-
-`
+  padding: 2em 5em;
+`;
 export const HeaderLink = styled.a`
-cursor: pointer;
-font-size: 14px;
-text-decoration: none !important;
-color: white;
-&:hover{
+  cursor: pointer;
+  font-size: 14px;
+  text-decoration: none !important;
+  color: white;
+  &:hover {
     opacity: 80%;
-}
-`
+  }
+`;
 
 export const HeaderContainer = styled.div`
-width: 100%;
-display:flex;
-justify-content: space-between;
-align-items: center;
-
-`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
 
 export const YourWishlistButton = styled.a`
-font-size: 14px;
-text-decoration: none !important;
-color: white;
-&:hover{
+  font-size: 14px;
+  text-decoration: none !important;
+  color: white;
+  &:hover {
     opacity: 80%;
-}
-    background-color: #5B606B;
-    padding: 0.8em 0.8em;
-    border-radius: 20px;
-`
+  }
+  background-color: #5b606b;
+  padding: 0.8em 0.8em;
+  border-radius: 20px;
+`;
 export const YourWishlistButtonContainer = styled.li`
-     display: flex;
-    align-items: center;
-     height: 2.5em;
-     border-left: 2px solid #303335;
-    padding: 0 1em;
-    cursor: pointer;
-    &:hover {
-        opacity: 80%;
-    }
- `
-
-
-
-
-
+  display: flex;
+  align-items: center;
+  height: 2.5em;
+  border-left: 2px solid #303335;
+  padding: 0 1em;
+  cursor: pointer;
+  &:hover {
+    opacity: 80%;
+  }
+`;
 
 export const Content = styled.div`
-     display:flex;
-     padding-left: 3em;
- `
-
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  align-items: flex-start;
+`;
+export const ContentBody = styled.div`
+    padding: 10px;
+    display: flex;
+    width: 90%;
+`
 
 export const SloganContainer = styled.div`
-      padding-top: 15em;
-      
-`
+  padding-top: 15em;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+`;
 
 export const TitleContainerSlogan = styled.div`
-     height: auto;
-     width: 36em;
-     h1{
-        color: white;
-     }
- `
+  height: auto;
+  width: 36em;
+  h1 {
+    padding: 10px;
+    font-size:45px;
+    color: white;
+  }
+`;
 export const StartButtonContainer = styled.div`
-     display:flex;
-     justify-content: center;
-     padding-top: 1em;
-`
+  display: flex;
+  justify-content: center;
+  padding-top: 1em;
+  width: 100%;
+`;
 export const StartButton = styled.button`
-     cursor: pointer;
-     font-size: 22px;
-     background-color: #2A41FA;
-     color: white;
-     padding: 0.5em 2em;
-     border-radius: 8px;
-     &:hover{
-        opacity: 80%;
-     }
-`
+  cursor: pointer;
+  width: 90%;
+  font-size: 22px;
+  background-color: #2a41fa;
+  color: white;
+  padding: 0.5em 2em;
+  border-radius: 8px;
+  &:hover {
+    opacity: 80%;
+  }
+`;
 export const Elemento = styled.div`
-     display: flex;
-     max-width: 100%;
-     padding-left: 45em;
-
-`
+  display: flex;
+  max-width: 100%;
+  padding-left: 45em;
+`;
 
 export const HIWContent = styled.div`
-    display: flex;
-    padding-top: 8em;
+  display: flex;
+  padding-top: 8em;
 
-    flex-direction: column;
-    align-items: center;
-        h1 {
-            color: #FFF;
-            color: #FFF;
-            font-family: Open Sans;
-            font-size: 40px;
-            font-style: normal;
-            font-weight: 700;
-            line-height: normal;
-        }
-`
+  flex-direction: column;
+  align-items: center;
+  h1 {
+    color: #fff;
+    color: #fff;
+    font-family: Open Sans;
+    font-size: 40px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+  }
+`;
 export const HIWText = styled.div`
-     width: 482px;
-     height: 100px;
-     padding-top: 3em;
-     padding-bottom: 10em;
-        p {
-            color: #FFF;
-            text-align: center;
-            font-family: Open Sans;
-            font-size: 16px;
-            font-style: normal;
-            font-weight: 700;
-            line-height: normal;
-        }
-`
+  width: 482px;
+  height: 100px;
+  padding-top: 3em;
+  padding-bottom: 10em;
+  p {
+    color: #fff;
+    text-align: center;
+    font-family: Open Sans;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+  }
+`;
 
 export const HIWImages = styled.div`
-        display: flex;
-        max-width: 100%;
-        display: flex;
-        flex-direction: row-reverse;
-        justify-content: space-around;
-        padding-bottom: 10em;
-`
+  display: flex;
+  max-width: 100%;
+  display: flex;
+  flex-direction: row-reverse;
+  justify-content: space-around;
+  padding-bottom: 10em;
+`;
 
 export const HIWImgContents = styled.div`
-        display: flex;
-        width: 311px:
-        height: 311px;
-        border-radius: 4px;
-        opacity: 85%
-`
+  display: flex;
+  width: 311px;
+  height: 311px;
+  border-radius: 4px;
+  opacity: 85%;
+`;
